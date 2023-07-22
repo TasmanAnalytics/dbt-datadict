2ee1e9862888d295bd6fc7e48b6a7be73648ea38
Tom Shelley <tom@tasman.ai>
2023-07-22 19:29:35 +0100

    add function documentation

--- datadict/datadict_helpers.py
+++ datadict/datadict_helpers.py
@@ -1,4 +1,16 @@
 def add_spaces_between_cols(file):
+    """
+    Add spaces between columns in a YAML file.
+
+    This function reads a YAML file and inserts newlines between the columns that start with '- name:'.
+    It then writes the modified content back to the same file.
+
+    Parameters:
+        file (str): The path to the YAML file to be processed.
+
+    Returns:
+        None
+    """
     with open(file, 'r') as f:
             yaml = str(f.read())
     replaced = yaml.replace('  - name:', '\n  - name:')
