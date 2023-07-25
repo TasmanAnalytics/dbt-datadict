aeb771953f6b3ceddae93628c9cf00fa1fe2cf48
Tom Shelley <tom@tasman.ai>
2023-07-25 21:26:37 +0100

    add new generate command to main script

--- datadict/datadict_main.py
+++ datadict/datadict_main.py
@@ -12,3 +12,9 @@ def apply(dictionary, directory):
     dictionary = datadict.datadict(dictionary, detailed_logs=True)
     dictionary.apply_data_dictionary_to_path(directory)
     dictionary.collate_output_dictionary()
+
+@cli.command()
+@click.option('-D', '--directory', type=str, help='Directory to apply dictionary', default='models/')
+@click.option('-n', '--name', type=str, help='Name to give the generated YAML file', default='models.yml')
+def generate(directory, name):
+    datadict.generate_model_yamls(directory, name)
