4c6814d93743035e32d5c1090344a38f10ba62f8
Tom Shelley <tom@tasman.ai>
2023-07-31 20:32:06 +0100

    improve function help text

--- datadict/datadict_main.py
+++ datadict/datadict_main.py
@@ -9,6 +9,11 @@ def cli():
 @click.option('-d', '--dictionary', type=str, help='Location of the dictionary file', default='datadictionary.yml')
 @click.option('-D', '--directory', type=str, help='Directory to apply dictionary', default='models/')
 def apply(dictionary, directory):
+    """
+    This command reviews all existing model files in the given directory for existing columns and collates them into a 
+    dictionary file. Additionally, this command will review the dictionary file and apply updates back to the columns in
+    the model files where possible.
+    """
     dictionary = datadict.datadict(dictionary, detailed_logs=True)
     dictionary.apply_data_dictionary_to_path(directory)
     dictionary.collate_output_dictionary()
@@ -18,4 +23,9 @@ def apply(dictionary, directory):
 @click.option('-f', '--file', type=str, help='Name to give the generated YAML file', default='models.yml')
 @click.option('--sort/--no-sort', help='Triggers the generated YAML files to be sorted alphabetically', default=False)
 def generate(directory, file, sort):
+    """
+    This command generates model YAML files in a specified directory. Existing model YAML files are evaluated,
+    and the model metadata is combined and written back to the existing files. For models missing from existing files,
+    a new file is created in the directory with the given name and the metadata for the missing models is written to it.
+    """
     datadict.generate_model_yamls(directory, file, sort)
