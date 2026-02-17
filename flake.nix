0b3c86077f8528b99505a83ddef65a8c21aca86d
Nima Isselee <nima@tasman.ai>
2026-02-17 11:25:26 +0000

    wip: Fix docs, remove make.

--- flake.nix
+++ flake.nix
@@ -51,8 +51,6 @@
               with pkgs;
               [
                 # Command runners
-                # For `make`
-                gnumake
                 just
 
                 # Used by command runners
@@ -80,6 +78,7 @@
               ]
             );
             shellHook = ''
+              # Github Actions sets the "CI" environment variable to true.
               if [ -z "''${CI:-}" ]; then
                 just ensure-uv-and-pre-commit
 
